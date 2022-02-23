05d492ee72db96d85884729e31550793081d47c7
Rinor Rama <rinor.rama95@gmail.com>
2022-02-23 21:09:41 +0100

    added initial data

---
+++ initialData.ts
@@ -0,0 +1,126 @@
+export const users = [
+  {
+    username: 'ramarinor',
+    password: '123456'
+  },
+  {
+    username: 'edputans',
+    password: 'putans'
+  },
+  {
+    username: 'nicolasmarcora',
+    password: 'luca2022'
+  },
+  {
+    username: 'lugageri',
+    password: '654321'
+  }
+];
+
+export const subreddits = [
+  {
+    name: 'subreddit1',
+    description: 'description of subreddit1'
+  },
+  {
+    name: 'subreddit2',
+    description: 'description of subreddit2'
+  },
+  {
+    name: 'subreddit3',
+    description: 'description of subreddit3'
+  },
+  {
+    name: 'subreddit4',
+    description: 'description of subreddit1'
+  }
+];
+
+export const posts = [
+  {
+    title: 'post 1',
+    content: 'the first post - by user 1 in subreddit 1',
+    userId: 1,
+    subredditId: 1
+  },
+  {
+    title: 'post 2',
+    content: 'the second post - by user 1 in subreddit 2',
+    userId: 1,
+    subredditId: 2
+  },
+  {
+    title: 'post 3',
+    content: 'the third post - by user 1 in subreddit 3',
+    userId: 1,
+    subredditId: 3
+  },
+  {
+    title: 'post 4',
+    content: 'the fourth post - by user 1 in subreddit 4',
+    userId: 1,
+    subredditId: 4
+  },
+  {
+    title: 'post 5',
+    content: 'the fifth post - by user 2 in subreddit 1',
+    userId: 2,
+    subredditId: 1
+  },
+  {
+    title: 'post 6',
+    content: 'the sixth post - by user 2 in subreddit 3',
+    userId: 2,
+    subredditId: 3
+  },
+  {
+    title: 'post 7',
+    content: 'the seventh post - by user 2 in subreddit 3',
+    userId: 2,
+    subredditId: 3
+  },
+  {
+    title: 'post 8',
+    content: 'the eighth pos - by user 3 in subreddit 1',
+    userId: 3,
+    subredditId: 1
+  },
+  {
+    title: 'post 9',
+    content: 'the ninth post - by user 3 in subreddit 2',
+    userId: 3,
+    subredditId: 2
+  },
+  {
+    title: 'post 10',
+    content: 'the tenth post - by user 3 in subreddit 4',
+    userId: 3,
+    subredditId: 4
+  }
+];
+
+export const comments = [
+  { content: 'hello', userId: 1, postId: 1 },
+  { content: 'from the', userId: 1, postId: 1 },
+  { content: 'other side', userId: 1, postId: 2 },
+  { content: 'i must', userId: 1, postId: 3 },
+  { content: 'have called', userId: 2, postId: 3 },
+  { content: 'a thousand', userId: 2, postId: 4 },
+  { content: 'times', userId: 3, postId: 4 },
+  { content: 'to tell you', userId: 3, postId: 3 },
+  { content: 'I am sorry', userId: 4, postId: 1 },
+  { content: 'for breaking your hear', userId: 4, postId: 4 }
+];
+
+export const votes = [
+  { userId: 1, postId: 1 },
+  { userId: 2, postId: 1 },
+  { userId: 3, postId: 1 },
+  { userId: 4, postId: 1 },
+  { userId: 1, postId: 2 },
+  { userId: 2, postId: 2 },
+  { userId: 4, postId: 2 },
+  { userId: 3, postId: 3 },
+  { userId: 4, postId: 3 },
+  { userId: 1, postId: 4 }
+];
--- setup.ts
+++ setup.ts
@@ -1,4 +1,5 @@
 import Database from 'better-sqlite3';
+import { users, subreddits, posts, comments, votes } from './initialData';
 
 const db = new Database('./data.db', {
   verbose: console.log
@@ -10,6 +11,8 @@ DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS subreddits;
+
+
 
 CREATE TABLE users (
     "id" INTEGER PRIMARY KEY,
@@ -50,3 +53,40 @@ CREATE TABLE users (
     FOREIGN KEY (userId) REFERENCES users(id)
   );
 `);
+
+const createUser = db.prepare(`
+  INSERT INTO users (username, password) VALUES (?, ?)
+`);
+const createSubreddit = db.prepare(`
+  INSERT INTO subreddits (name, description) VALUES (?, ?)
+`);
+const createPost = db.prepare(`
+  INSERT INTO posts (title, content, userId, subredditId) VALUES (?, ?, ?, ?)
+`);
+const createComment = db.prepare(`
+  INSERT INTO comments (content, userId, postId) VALUES (?, ?, ?)
+`);
+
+const createVote = db.prepare(`
+  INSERT INTO votes (userId, postId) VALUES (?, ?)
+`);
+
+for (const user of users) {
+  createUser.run(user.username, user.password);
+}
+
+for (const subreddit of subreddits) {
+  createSubreddit.run(subreddit.name, subreddit.description);
+}
+
+for (const post of posts) {
+  createPost.run(post.title, post.content, post.userId, post.subredditId);
+}
+
+for (const comment of comments) {
+  createComment.run(comment.content, comment.userId, comment.postId);
+}
+
+for (const vote of votes) {
+  createVote.run(vote.userId, vote.postId);
+}
